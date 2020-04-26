168fd4cb85992ac79a55dde5e78b65b23dc9b0dd
TAKANO Mitsuhiro <takano32@gmail.com>
2020-04-26 09:04:46 +0900

    add run.sh

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# vim: set ft=sh ff=unix fileencoding=utf-8 expandtab ts=2 sw=2 :
+# ref. https://blog.kamipo.net/entry/2013/02/20/122225
+#
+
+if which python3 > /dev/null 2>&1; then
+  # echo python3
+  true
+fi
+
+if which ngrok > /dev/null 2>&1; then
+  # echo ngrok
+  true
+fi
+
+python3 -m http.server 5000 > /dev/null 2>&1 &
+PYTHON_PID=$!
+
+ngrok http 5000
+
+# trap "kill $PYTHON_PID # kill python3" 1 2 3 15
+# trap -p
+kill $PYTHON_PID
+
